052f63ffe4c2528aef920652652272ec02061134
bentrueman <benjamin.trueman@dal.ca>
2022-09-02 10:25:04 -0300

    new arg to local_slope()

--- R/local_slope.R
+++ R/local_slope.R
@@ -8,6 +8,9 @@
 #' @param smooth The smooth term for which to calculate local slopes. Passed on to `brms::posterior_smooths()`.
 #' @param ... Additional arguments passed to `brms::posterior_smooths()`.
 #' @param g_var An optional grouping variable for factor-smooth interactions.
+#' @param add_vars A named list containing variables to add as columns to the dataframe passed to `brms::posterior_smooths()` as `newdata`.
+#' Useful when the smooth specified by `smooth` does not include the variables used to construct the CAR(1) term
+#' (causing an error in `posterior_smooths()`).
 #'
 #' @return A `tibble` containing the (tidy) output of `brms::posterior_smooth()` and the calculated slopes.
 #' @importFrom brms posterior_smooths
@@ -21,16 +24,16 @@
 #' seed <- 1
 #' data_gam <- read.csv(paste0(system.file("extdata", package = "bgamcar1"), "/data_gam.csv"))
 #' fit <- fit_stan_model(
-#'    paste0(system.file("extdata", package = "bgamcar1"), "/test_gam"),
-#'    seed,
-#'    bf(y ~ s(x0) + s(x1) + s(x2) + s(x3)),
-#'    data_gam,
-#'    car1 = FALSE,
-#'    chains = 2
-#'  )
+#'   paste0(system.file("extdata", package = "bgamcar1"), "/test_gam"),
+#'   seed,
+#'   bf(y ~ s(x0) + s(x1) + s(x2) + s(x3)),
+#'   data_gam,
+#'   car1 = FALSE,
+#'   chains = 2
+#' )
 #' local_slope(data_gam, fit, "x2", smooth = "s(x2)")
-
-local_slope <- function(input, object, x_var, epsilon = .001, smooth, g_var = NULL, ...) {
+#'
+local_slope <- function(input, object, x_var, epsilon = .001, smooth, g_var = NULL, add_vars = NULL, ...) {
 
   grid_1 <- data.frame(
     seq(
@@ -40,11 +43,20 @@ local_slope <- function(input, object, x_var, epsilon = .001, smooth, g_var = NU
     )
   )
 
+  message(paste0(paste0("Evaluating at ", dim(grid_1)[1]), " points (per group)"))
+
   names(grid_1) <- x_var
 
-  if(!is.null(g_var)) {
+  if (!is.null(g_var)) {
     g <- unique(input[, g_var])
     grid_1 <- crossing(g, grid_1)
+  }
+
+  # add variables to grid to satisfy brms checks on ts models:
+  if (!is.null(add_vars)) {
+    for (i in 1:length(add_vars)) {
+      grid_1[, names(add_vars)[i]] <- add_vars[[i]]
+    }
   }
 
   grid_2 <- grid_1
@@ -53,17 +65,13 @@ local_slope <- function(input, object, x_var, epsilon = .001, smooth, g_var = NU
   grid_2[, x_var] <- grid_1[, x_var] + epsilon
   grid_avg[, x_var] <- (grid_1[, x_var] + grid_2[, x_var]) / 2
 
-  # if (is.null(g_var)) {
-  #   smooth_1 <- posterior_smooths(object, smooth = smooth, newdata = grid_1, ...)
-  #   smooth_2 <- posterior_smooths(object, smooth = smooth, newdata = grid_2, ...)
-  #   smooth_avg <- posterior_smooths(object, smooth = smooth, newdata = grid_avg, ...)
-  # } else {
-  #   smooth_1 <- map_post_smooth(grid_1, object, smooth, g_var, ...)
-  #   smooth_2 <- map_post_smooth(grid_2, object, smooth, g_var, ...)
-  #   smooth_avg <- map_post_smooth(grid_avg, object, smooth, g_var, ...)
-  # }
-
-  smooth_1 <- posterior_smooths(object, smooth = smooth, newdata = grid_1, ...)
+  smooth_1 <- try(posterior_smooths(object, smooth = smooth, newdata = grid_1, ...))
+  if (!is.matrix(smooth_1)) {
+    if (smooth_1 == "Error : Time points within groups must be unique.\n") {
+      stop("One or more variables used to construct CAR(1) term not used in the smooth term.
+           Add them via `add_vars` to satisfy brms::posterior_smooths()")
+    }
+  }
   smooth_2 <- posterior_smooths(object, smooth = smooth, newdata = grid_2, ...)
   smooth_avg <- posterior_smooths(object, smooth = smooth, newdata = grid_avg, ...)
 
@@ -82,13 +90,4 @@ local_slope <- function(input, object, x_var, epsilon = .001, smooth, g_var = NU
   derivs_df$smooth <- smooths_df$smooth
 
   derivs_df
-
-}
-
-map_post_smooth <- function(x, object, smooth, g_var, ...) {
-  smooths <- lapply(
-    as.list(unique(x[, g_var])),
-    \(y) posterior_smooths(object, smooth, newdata = x[x[, g_var] == y, ], ...)
-  )
-  do.call(cbind, smooths)
 }
--- tests/testthat/test-local_slope.R
+++ tests/testthat/test-local_slope.R
@@ -1,5 +1,6 @@
 
 library("brms")
+library("dplyr")
 
 seed <- 1
 
@@ -25,9 +26,22 @@ fit2 <- fit_stan_model(
   chains = 2
 )
 
+fit3 <- fit_stan_model(
+  paste0(system.file("extdata", package = "bgamcar1"), "/test_gam2"),
+  seed,
+  bf(y ~ s(x0, by = g) + s(x1, by = g) + s(x2, by = g) + s(x3, by = g) + ar(time = x1, gr = g)),
+  data_gam2,
+  save_warmup = FALSE,
+  car1 = FALSE,
+  chains = 2
+)
+
 slopes <- local_slope(data_gam, fit, "x2", smooth = "s(x2)")
 slopes2 <- local_slope(data_gam2, fit2, "x2", smooth = "s(x2, by = g)", g_var = "g")
-smooths <- map_post_smooth(data_gam2, object = fit2, smooth = "s(x2, by = g)", g_var = "g")
+slopes3 <- local_slope(data_gam2, fit2, "x2", smooth = "s(x2, by = g)", add_vars = list(g = "b"))
+slopes4 <- local_slope(
+  data_gam2, fit3, "x2", smooth = "s(x2, by = g)", g_var = "g", add_vars = list(x1 = 1:(459 * 3))
+)
 
 test_that("local_slope() returns expected values", {
   expect_snapshot(slopes)
@@ -37,16 +51,34 @@ test_that("local_slope() returns expected values for factor-smooth interaction",
   expect_snapshot(slopes2)
 })
 
-test_that("unexported function map_post_smooth() returns expected values for factor-smooth interaction", {
-  expect_snapshot(smooths)
+test_that("add_vars argument generates the same output as g_var", {
+  sm2 <- slopes2 %>%
+    filter(g == "b")
+  sm3 <- slopes3 %>%
+    relocate(g, .before = x2)
+  expect_equal(sm2, sm3)
 })
 
-test_that("unexported function map_post_smooth() matches brms::posterior_smooths()", {
-  smooths2 <- posterior_smooths(fit2, smooth = "s(x2, by = g)", newdata = data_gam2)
-  expect_equal(smooths, smooths2)
+test_that("local_slope() add_vars argument returns same result as g_var argument", {
+  sm2 <- slopes2 %>%
+    filter(g == "b")
+  sm3 <- slopes3 %>%
+    relocate(g, .before = x2)
+  expect_equal(sm2, sm3)
 })
 
+test_that("local_slope() returns an error when smooth term does not include CAR(1) variables", {
+  expect_error(
+    local_slope(data_gam2, fit3, "x2", smooth = "s(x2, by = g)", g_var = "g"),
+    regexp = "One or more variables used to construct CAR\\(1\\) term"
+  )
+})
 
-
+test_that("local_slope() argument add_var doesn't change results when added (arbitrary)
+variable not included in smooth (and is instead added to satisfy brms::posterior_smooths() requirement
+          that variables used in brms::ar() also present in newdata)", {
+  expect_equal(slopes2$smooth, slopes4$smooth)
+  expect_equal(slopes2$slope, slopes4$slope)
+})
 
 
